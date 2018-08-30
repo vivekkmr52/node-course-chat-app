918fb8bb6f583b801a8ec6158333576a5873f81d
Vivek Kumar <vivekkmr527@gmail.com>
2018-08-30 18:11:03 +0530

    Add autoscroll if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,19 @@
 var socket = io();
 
+function scrollToBottom () {
+  let messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child')
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight +scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function() {
   console.log('connected to server');
 
@@ -19,6 +33,7 @@ socket.on('newMessage', function(message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 
   // var formattedTime = moment(message.createdAt).format('h:mm a');
   // console.log('New Message', message);
@@ -46,6 +61,7 @@ socket.on('newLocationMessage', function(message) {
   // a.attr('href',message.url);
   // li.append(a);
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
   jQuery('#message-form').on('submit',function (e) {
